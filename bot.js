c6de5031345b3b6a069b74741faa4f2ca8489fe6
Abstr4 <matias.rojas@mi.unc.edu.ar>
2025-02-24 23:01:18 -0300

    Database connection

--- bot.js
+++ bot.js
@@ -84,7 +84,7 @@ client.on('messageCreate', async (message) => {
 
         // Query the database for the command
         connection.query(
-            'SELECT response FROM ExclamationCommands WHERE Command = ?',
+            'SELECT Response FROM ExclamationCommands WHERE Command = ?',
             [commandName],
             (err, results) => {
                 if (err) {
