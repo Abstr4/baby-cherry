adf1eb87031aed130c3bf536010a32959002aac6
Abstr4 <matias.rojas@mi.unc.edu.ar>
2025-02-27 16:01:02 -0300

    fix/events command

--- commands/slash/Event/events.js
+++ commands/slash/Event/events.js
@@ -1,5 +1,5 @@
 require('module-alias/register');
-const database = require('@database')
+const database = require('@database');
 const { SlashCommandBuilder } = require('discord.js');
 
 module.exports = {
@@ -9,13 +9,17 @@ module.exports = {
 
     async execute(interaction) {
         try {
-            const [rows] = await database.execute("SELECT ID, Message, EventAt FROM Event");
+            const [rows] = await database.execute("SELECT ID, Message, EventAt, ChannelId, RoleId FROM Event");
 
             if (rows.length === 0) {
                 return interaction.reply({ content: 'No events found.', ephemeral: true });
             }
 
-            const eventList = rows.map(event => `• **ID:** ${event.ID} | **${event.Message}** - ${event.RemindAt}`).join('\n');
+            const eventList = rows.map(event => 
+                `• **ID:** ${event.ID} | **${event.Message}** - <t:${Math.floor(new Date(event.EventAt).getTime() / 1000)}:F> ` +
+                `in <#${event.ChannelId}> ${event.RoleId ? `for <@&${event.RoleId}>` : ""}`
+            ).join('\n');
+
             await interaction.reply({ content: eventList, ephemeral: true });
         } catch (error) {
             console.error(error);
