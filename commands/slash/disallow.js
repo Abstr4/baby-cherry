fcf63430cb012957588c7005b659c3f14a3719c0
Abstr4 <matias.rojas@mi.unc.edu.ar>
2025-02-28 00:16:57 -0300

    allowlist fix

--- commands/slash/disallow.js
+++ commands/slash/disallow.js
@@ -1,5 +1,6 @@
 const { SlashCommandBuilder } = require('discord.js');
 const database = require('@database');
+const { allowlist } = require('../handlers/slashCommands.js'); // Import allowlist
 
 module.exports = {
     data: new SlashCommandBuilder()
