a85054f5aa0383facfbd6c2915b1f56aabfe40c1
Abstr4 <matias.rojas@mi.unc.edu.ar>
2025-04-15 13:50:01 -0300

    refactor/allow.js with messageService

--- commands/slash/Allow/allow.js
+++ commands/slash/Allow/allow.js
@@ -3,7 +3,7 @@ const { SlashCommandBuilder } = require('discord.js');
 const database = require('@database');
 const { allowList } = require('@root/commands/handlers/slashCommands.js');
 const { sendNoPermissionMessage, isAdmin } = require('@helpers');
-const { sendEphemeralMessage, sendMessage } = require('@messageService');
+const { sendEphemeralMessage } = require('@messageService');
 
 module.exports = {
     data: new SlashCommandBuilder()
@@ -17,7 +17,6 @@ module.exports = {
 
     async execute(interaction) {
         try {
-            // Check if the user has Administrator permission
             if (!isAdmin(interaction)) {
                 return sendNoPermissionMessage(interaction);
             }
@@ -27,20 +26,17 @@ module.exports = {
             const [rows] = await database.execute("SELECT 1 FROM Allowlist WHERE user_id = ?", [user.id]);
             if (rows.length > 0) {
                 return sendEphemeralMessage(interaction, `${user} is already in the allowlist.`);
-                // return interaction.reply({ content: `${user} is already in the allowlist.`, flags: 64 });
             }
 
             // Add it
             await database.execute("INSERT IGNORE INTO Allowlist (user_id) VALUES (?)", [user.id]);
             allowList.add(user.id);
 
-            return sendEphemeralMessage(interaction, `${user.username} is now allowed to use commands!`);
-            // return interaction.reply({ content: `${user.username} is now allowed to use commands!`, flags: 64 });
+            return sendEphemeralMessage(interaction, `${user} is now allowed to use commands!`);
         }
         catch (error) {
             console.error(error);
             return sendEphemeralMessage(interaction, "An error occurred while allowing the user.");
-            // return interaction.reply({ content: "An error occurred while allowing the user.", flags: 64 });
         }
     }
 };
