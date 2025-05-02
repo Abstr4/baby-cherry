fd287f82ee08b0a0976ae967503b7a39c8fe7fe6
Abstr4 <matias.rojas@mi.unc.edu.ar>
2025-05-02 12:42:18 -0300

    fix/buttonInteractionHandler function in bot.js

--- bot.js
+++ bot.js
@@ -13,7 +13,7 @@ const slashCommands = require('./commands/slash/slash.js');
 const loadSlashCommands = require('./helpers/loadSlashCommands.js');
 const { handleSlashCommand, loadAllowList } = require('./commands/handlers/slashCommands');
 const { handleLandMessage } = require('./commands/handlers/landMessageHandler.js');
-const buttonInteractionHandler = require("./commands/handlers/buttonInteractionHandler");
+const buttonInteractionHandler = require("./commands/handlers/buttonInteractionHandler.js");
 
 const client = new Client({
     intents: [
@@ -60,7 +60,7 @@ client.on('interactionCreate', async (interaction) => {
     if (interaction.user.bot) return;
     await handleSlashCommand(interaction, client);
     if (interaction.isButton()) {
-        await handleButtonInteraction(interaction);
+        await buttonInteractionHandler(interaction);
     }
 });
 
